1f7a4b0034e1c11420b973ff29ed39ccc926feb5
fazalfarhan01 <fazal.farhan@gmail.com>
2021-02-06 20:49:06 +0530

    updating for timer

--- src/main.cpp
+++ src/main.cpp
@@ -26,49 +26,89 @@
 #include <OTAFunctions.h>
 /******************************INCLUDES DONE*******************************/
 
+#define BLYNK_PRINT Serial
 
 BLYNK_WRITE(V0)
 {
-  digitalWrite(D4,!param.asInt());
+  digitalWrite(D4, !param.asInt());
 }
 BLYNK_WRITE(V1)
 {
-  digitalWrite(D3,!param.asInt());
+  digitalWrite(D3, !param.asInt());
 }
 BLYNK_WRITE(V2)
 {
-  digitalWrite(D2,!param.asInt());
+  digitalWrite(D2, !param.asInt());
 }
 BLYNK_WRITE(V3)
 {
-  digitalWrite(D1,!param.asInt());
+  digitalWrite(D1, !param.asInt());
 }
 BLYNK_WRITE(V4)
 {
-  digitalWrite(D5,!param.asInt());
+  digitalWrite(D5, !param.asInt());
+}
+BLYNK_WRITE(V12)
+{
+  if (!param.asInt() >= 100)
+  {
+    ESP.restart();
+  }
 }
 
-BLYNK_CONNECTED(){
+BLYNK_CONNECTED()
+{
   Blynk.syncVirtual(V0, V1, V2, V3, V4);
 }
 
+void sendUpTime()
+{
+  long uptime = millis() / 1000;
+  Serial.println("Sending Uptime: " + String(uptime));
+  Blynk.virtualWrite(V10, uptime);
+}
 
+void sendWiFiStrength()
+{
+  long wifiStrength = WiFi.RSSI();
+  Serial.println("Sending Signal Strength: " + String(wifiStrength));
+  Blynk.virtualWrite(V11, wifiStrength);
+}
+
+unsigned long currentMillis = millis();
+unsigned long previousMillis = 0;
+void customTimer(unsigned int timeInMillisec)
+{
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= timeInMillisec)
+  {
+    previousMillis = currentMillis;
+    // Put functions to be triggered after timer
+    sendWiFiStrength();
+    sendUpTime();
+  }
+}
 
 void yourSetup()
 {
   Blynk.begin(auth, ssid, password, serverName, 8080);
+  Serial.println("NodeMCU is now connected..!");
+  Blynk.notify("Yaaay... NodeMCU is now online..!");
+  Serial.println("Defining pinModes");
   pinMode(D1, OUTPUT);
   pinMode(D2, OUTPUT);
   pinMode(D3, OUTPUT);
   pinMode(D4, OUTPUT);
   pinMode(D5, OUTPUT);
+  Serial.println("Setting up timers..!");
+  Blynk.virtualWrite(V12, 0);
 }
 
 void yourLoop()
 {
   Blynk.run();
+  customTimer(1000);
 }
-
 
 void setup()
 {
